45b3a8e37e36a5c7a015302019ef8dfc44c79d6c
Ka1phas <kaiphas@me.com>
2016-06-23 19:05:14 +0200

    Server now sends weather data(temp) as json response

--- raspberrypi/server.py
+++ raspberrypi/server.py
@@ -2,11 +2,26 @@
 
 import json
 import requests
+import time
 from bottle import route, run
 
 @route('/weather')
 def weather():
-	return str(time)#getJsonFromFile('weather.json')
+	now = int(time.time())
+	lastqueried = getJsonFromFile('time.json')
+	last = int(lastqueried['time'])
+	difference = now - last
+	if difference > 600:
+		print('current weather data is older than 10 minutes -- fetching from server')
+		getOnlineWeatherData(2934691)
+		f = open('time.json', 'w')
+		try:
+			json.dump({"time": str(now)}, f)
+		finally:
+			f.close()
+	# Now return actual weather data
+	weatherData = getLocalWeatherData()
+	return str(weatherData)#getJsonFromFile('weather.json')
 
 # This function returns a json Object from a specified file
 def getJsonFromFile( fileName ):
@@ -14,10 +29,17 @@ def getJsonFromFile( fileName ):
     data = json.load(data_file)
     return data
 
-def getOnlineWeatherData( cityID ):
-	url = "http://api.openweathermap.org/data/2.5/weather?id=" + str(cityID) +"2860410&APPID=154e9a2853bb39627c17906e69f7a56c&units=metric"
+def getOnlineWeatherData( cityID ): #Duisburg 2934691
+	url = "http://api.openweathermap.org/data/2.5/weather?id=" + str(cityID) +"&APPID=154e9a2853bb39627c17906e69f7a56c&units=metric"
 	r = requests.get(url)
 	weather = r.json()
-	json.dump(data, open('weather.json', 'w'))
+	f = open('weather.json', 'w')
+	try:
+		json.dump(weather, f)
+	finally:
+		f.close
+def getLocalWeatherData():
+	data = getJsonFromFile('weather.json')
+	return {"temperatur": data["main"]["temp"]}
 
 run(host='localhost', port=3000, debug=True)
